732f870e1efa7446802b25799d7a0a6c5653e52e
Nicolas Huray <nicolas.huray@1science.org>
2015-09-22 11:12:03 -0400

    Minor

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.getLogger("botocore").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 h1 = lambda x: logger.info("\033[1m\033[4m\033[94m%s\033[0m\n" % x)
 success = lambda x: logger.info("\033[92m✔ %s\033[0m\n" % x)
-error = lambda x: logger.info("\033[91m ✖ %s\033[0m\n" % x)
+error = lambda x: logger.info("\033[91m✖ %s\033[0m\n" % x)
 
 # Arguments parsing
 parser = argparse.ArgumentParser(description='Deploy Service on ECS')
